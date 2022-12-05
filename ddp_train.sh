545cbc65d5f866880489b322ac781983eff54316
Hyejin-Koo <9a3710@naver.com>
2022-12-05 15:58:32 +0900

    multigpu training shell

--- ddp_train.sh
+++ ddp_train.sh
@@ -1,7 +1,10 @@
-python s3prl/run_downstream.py -m train \
+gpus=2;
+distributesd='-m torch.distributed.launch --nproc_per_node ${gpus}'
+
+python $distributed s3prl/run_downstream.py -m train \
 	-d enhancement_stft2 \
 	-u wavlm \
-	-f \
+	-f -o config.runner.gradient_accumulate_steps=2 \
 	-e result/downstream/unfreeze/best-states-dev.ckpt \
 #	-c s3prl/downstream/enhancement_stft2/configs/cfg_voicebank.yaml \
 #	-n wav2vec \
