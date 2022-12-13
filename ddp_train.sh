6f09df7300aa10221493a20fa47b4b066ab0f83d
Hyejin-Koo <9a3710@naver.com>
2022-12-13 13:34:33 +0900

    ddp training code

--- ddp_train.sh
+++ ddp_train.sh
@@ -1,12 +1,14 @@
 gpus=2;
-distributesd='-m torch.distributed.launch --nproc_per_node ${gpus}'
+distributed="-m torch.distributed.launch --nproc_per_node=${gpus}"
 
 python $distributed s3prl/run_downstream.py -m train \
 	-d enhancement_stft2 \
 	-u wavlm \
-	-f -o config.runner.gradient_accumulate_steps=2 \
-	-e result/downstream/unfreeze/best-states-dev.ckpt \
-#	-c s3prl/downstream/enhancement_stft2/configs/cfg_voicebank.yaml \
+	-n ddp \
+	-c s3prl/downstream/enhancement_stft2/configs/cfg_voicebank.yaml \
+	-f \
+#	--local_rank 0
+#	-e result/downstream/unfreeze/best-states-dev.ckpt \
 #	-n wav2vec \
 #	-g 's3prl/upstream/log_stft/stft_mag.yaml'\
 #	-e result/downstream/wavlm-768/best-states-dev.ckpt
